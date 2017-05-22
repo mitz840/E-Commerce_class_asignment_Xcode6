3777b5c4a35fdcd57c5f040b6cb4d465566fe524
Mitesh Patel <mitz840@GMAIL.COM>
2017-05-22 19:50:35 -0400

    Add files via upload

---
+++ Miteshkumar_patel_C0671017_ASSIGNMENTTests/Miteshkumar_patel_C0671017_ASSIGNMENTTests.swift
@@ -0,0 +1,36 @@
+//
+//  Miteshkumar_patel_C0671017_ASSIGNMENTTests.swift
+//  Miteshkumar_patel_C0671017_ASSIGNMENTTests
+//
+//  Created by Mitesh Patel on 2016-03-25.
+//  Copyright (c) 2016 Miteshkumar Patel. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Miteshkumar_patel_C0671017_ASSIGNMENTTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
